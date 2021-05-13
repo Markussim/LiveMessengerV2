02ec8573c7d798d06c7de24258c7685739e97e46
Linus Romland <romlandlinus@gmail.com>
2021-05-13 10:58:35 +0200

    Commented GetRoom.cs

--- server/LiveMessenger/GetRoom.cs
+++ server/LiveMessenger/GetRoom.cs
@@ -9,12 +9,14 @@ namespace LiveMessenger
 {
     public class GetRoom
     {
-        public static List<BsonDocument> RetrieveAllRooms()
+        //Function that retrieves all entries in the MongoDB Collection "Rooms" without any filter
+        public static List<BsonDocument> RetrieveAllRooms() 
         {
             var collection = ConnectToDB.db.GetCollection<BsonDocument>("Rooms");
             return collection.Find(new BsonDocument()).ToList();
         }
 
+        //Function that retrieve on specific room with Filter of RoomID from the MongoDB Collection "Rooms"
         public static BsonDocument RetrieveOneRoom(string roomID){
             var collection = ConnectToDB.db.GetCollection<BsonDocument>("Rooms");
             var filter = Builders<BsonDocument>.Filter.Eq("_id", ObjectId.Parse(roomID));
