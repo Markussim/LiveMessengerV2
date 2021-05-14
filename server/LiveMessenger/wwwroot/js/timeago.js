b453bb90f56aca10cedd4c2628008f44092f0018
Linus Romland <romlandlinus@gmail.com>
2021-05-14 11:05:29 +0200

    added tooltip on hover of date to display exact date

--- server/LiveMessenger/wwwroot/js/timeago.js
+++ server/LiveMessenger/wwwroot/js/timeago.js
@@ -3,6 +3,7 @@ function renderTime() {
 
 	TimeAgo.forEach((element) => {
 		element.innerHTML = moment(element.attributes[1].value).fromNow();
+		element.setAttribute("title", moment(element.attributes[1].value).format("dddd, MMMM Do YYYY, h:mm"))
 	});
 }
 renderTime()
