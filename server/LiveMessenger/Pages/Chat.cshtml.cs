51c0a3931ceb8dff9ddfc881b981d2615e965d0f
Linus Romland <romlandlinus@gmail.com>
2021-05-14 10:46:59 +0200

    started on chat UI design

--- server/LiveMessenger/Pages/Chat.cshtml.cs
+++ server/LiveMessenger/Pages/Chat.cshtml.cs
@@ -14,12 +14,15 @@ namespace LiveMessenger.Pages
     public class ChatModel : PageModel
     {
         public List<BsonDocument> previousMessages { get; set; }
+
+        public BsonDocument room { get; set; }
         public IActionResult OnGet()
         {
             if (!CheckCookie.checkUsername(Request)) return Redirect("ChangeUsername");
             String id = Request.Query["id"];
             if (!CheckRoom.byID(id)) return Redirect("/");
-            if (GetRoom.RetrieveOneRoom(id).GetElement("Private").Value == true && !CheckPassword.isCorrect(Request.Cookies[id], id)) return Redirect($"/PasswordCheck?id={id}");
+            room = GetRoom.RetrieveOneRoom(id);
+            if (room.GetElement("Private").Value == true && !CheckPassword.isCorrect(Request.Cookies[id], id)) return Redirect($"/PasswordCheck?id={id}");
             previousMessages = GetPreviousMessages.RetrievePreviousMessages(id);
             return null;
         }
